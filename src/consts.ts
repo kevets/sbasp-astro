392bafcf30171ffc01bc935db3e2737b88484f80
Steve King <kevets@gmail.com>
2025-08-25 18:58:21 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "News and Guidance show";
+export const SITE_DESCRIPTION = "NAG is a notebookLM collaboration.";
