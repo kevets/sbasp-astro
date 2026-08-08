db7bea40744bf1a332dbf1d617e1d90ac8f070f6
Steve King <kevets@gmail.com>
2026-08-08 14:02:12 -0500

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static',
-  site: 'https://kevets.github.io',
-  base: '/sbasp-astro',
-});
